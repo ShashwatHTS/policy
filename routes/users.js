ab15e60475a44050dd29cd798ba3cfc39e0ebf08
shashwatHTS <shashwatpal@htstechsolutions.com>
2024-07-17 12:26:15 +0530

    image name updated

--- routes/users.js
+++ routes/users.js
@@ -62,7 +62,17 @@ async function uploadArrayBufferToSupabase(arrayBuffer, destinationPath) {
 const uploadImg = async (req, res) => {
   try {
     const file = req.file.buffer
-    const fileName = req.file.originalname;
+    let fileName = req.file.originalname;
+    let date_time = new Date();
+    let date = ("0" + date_time.getDate()).slice(-2);
+    let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+    let year = date_time.getFullYear();
+    let hours = date_time.getHours();
+    let minutes = date_time.getMinutes();
+    let seconds = date_time.getSeconds();
+    console.log(year + "-" + month + "-" + date);
+    console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
+    fileName = `Date-${year}-${month}-${date}-Time-${hours}-${minutes}-${seconds}-${fileName}`
 
     console.log("fileName", fileName)
 
